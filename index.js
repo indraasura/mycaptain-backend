a14e40e5c729ce9a59cf0b83d120accccd97b38b
Swarup Hegde <swaruphegde@gmail.com>
2020-03-24 20:42:25 +0530

    feat: post routes for workshops

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const Joi = require('joi')
+app.use(express.json())
 
 workshops = [
     {id: 1, name: 'workshop1'},
@@ -12,16 +14,43 @@ workshops = [
 
 
 app.get('/', (req, res) => {
-    res.send('')
+    res.send('Hello World')
 })
 
-app.get('/workshops', (req, res) => {
+app.get('/api/workshops', (req, res) => {
     res.send(workshops)
 })
 
-app.get('/workshops/:id', (req, res) => {
-    
+app.get('/api/workshops/:id', (req, res) => {
+    const workshop = workshops.find(w => parseInt(req.params.id) === w.id)
+    if (!workshop) return res.status(404).send("The requested workshop was not found")
+    res.send(workshop)
 })
+
+// Post request
+app.post('/api/workshops', (req, res) => {
+    
+    const { error } = validateCourse(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    workshop = {
+        id: workshops.length + 1,
+        name: req.body.name
+    }
+    workshops.push(workshop)
+    res.send(workshops)
+})
+
+validateCourse = workshop => {
+    const schema = {
+        name : Joi.string().min(3).required()
+    }
+    return Joi.validate(workshop, schema)
+}
+
 
 
 app.listen(3000)
+
+
+
+
