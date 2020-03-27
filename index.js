e5abe1234979d7ca482f79be4bd8aaa1d9df7c6f
Swarup Hegde <swaruphegde@gmail.com>
2020-03-27 21:03:24 +0530

    feat: put and delete

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ workshops = [
     {id: 3, name: 'workshop3'}
 ]
 
+app.set('view-engine', 'ejs')
 
 // Get routes
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home.ejs')
 })
 
 app.get('/api/workshops', (req, res) => {
@@ -29,7 +30,6 @@ app.get('/api/workshops/:id', (req, res) => {
 
 // Post request
 app.post('/api/workshops', (req, res) => {
-    
     const { error } = validateCourse(req.body)
     if(error) return res.status(400).send(error.details[0].message)
     workshop = {
@@ -37,6 +37,30 @@ app.post('/api/workshops', (req, res) => {
         name: req.body.name
     }
     workshops.push(workshop)
+    res.send(workshops)
+})
+
+
+// Put request
+app.put('/api/workshops/:id', (req, res) => {
+    // requested workshop exists?
+    const workshop = workshops.find(w => parseInt(req.params.id) === w.id)
+    if (!workshop) return res.status(404).send("The requested workshop was not found")
+    // check the req body to see if the entered name is valid or not
+    const { error } = validateCourse(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    // update the workshop's name
+    workshop.name = req.body.name
+    // return to the user
+    res.send(workshops)
+})
+
+// Delete request
+app.delete('/api/workshops/:id', (req, res) => {
+    const workshop = workshops.find(w => parseInt(req.params.id) === w.id)
+    if (!workshop) return res.status(404).send("The requested workshop was not found")
+    const index = workshops.indexOf(workshop)
+    workshops.splice(index, 1)
     res.send(workshops)
 })
 
